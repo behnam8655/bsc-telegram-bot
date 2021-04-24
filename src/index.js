7a864639487a2c3a81011b24a6fba10d5733fb79
Marco Bertolino <marcobertolino.developer@gmail.com>
2021-04-24 12:20:21 +0200

    small fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const config = require("./commons/config.json");
 const { updatePrices } = require("./libs/prices")
 
 app.post("/", async (req, res) => {
-    console.log("🚀 ~ file: index.js ~ line 12 ~ app.post ~  req.body", req.body)
     if (req.body.update_id) res.status(200).send();
     console.log("received: " + req.body.message.text + " from " + req.body.message.chat.first_name + " " + req.body.message.chat.username)
     await bot.handleUpdate(req.body, res).catch(
